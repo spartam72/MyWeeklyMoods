ff2472b969d7eddd5a126589dd313414902a5a78
spartam72 <spartam72@yahoo.fr>
2018-12-07 14:43:11 +0100

    add Constant class

---
+++ app/src/main/java/com/example/spart/myweeklymoods/tools/Constant.java
@@ -0,0 +1,8 @@
+package com.example.spart.myweeklymoods.tools;
+
+public class Constant {
+
+    public static String TAG = "activity_main";
+    public static final String historicMoods = "historicMood";
+    public static final String userhHistoric = "userhHistoric ";
+}
